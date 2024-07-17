911a2bad6ee3d4685b93dc251ebad4591b0c549d
Shi Jie Ng <142388278+SJieNg123@users.noreply.github.com>
2024-07-17 13:42:55 +0800

    Update 121 Best Time to Buy and Sell Stock.c

--- Leetcode/121 Best Time to Buy and Sell Stock.c
+++ Leetcode/121 Best Time to Buy and Sell Stock.c
@@ -1,6 +1,10 @@
 int maxProfit(int* prices, int pricesSize) {
-    //int profit = 0;
+
+    // brute force solution
+    // code below will face TLE since the time complexity is O(n^2)
+    
     /*
+    int profit = 0;
     for(int i=pricesSize-1; i>0; i--)
     {
         for(int j=i-1; j>=0; j--)
@@ -14,6 +18,7 @@ int maxProfit(int* prices, int pricesSize) {
     }
     */
 
+    // code below reduce time complexity to O(n), so it will pass all testcases
     int ans = 0, min = 10000;
     for(int i=0; i<pricesSize; i++)
     {
