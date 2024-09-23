53501152840725338d6c1809c923d904a5c7c994
Shi Jie Ng <142388278+SJieNg123@users.noreply.github.com>
2024-09-23 21:39:18 +0800

    Delete Leetcode Solutions/Problem0231 - Power of Two.cpp

--- Leetcode Solutions/Problem0231 - Power of Two.cpp
+++
@@ -1,8 +0,0 @@
-class Solution {
-public:
-    bool isPowerOfTwo(int n) {
-        if(n == 1) return true;
-        if(n % 2 != 0 || n <= 0) return false;
-        return isPowerOfTwo(n/2);
-    }
-};
